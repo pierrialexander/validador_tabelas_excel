22dc1a1b03ece10bf73e81bb36618a9c704fcf5b
Pierri Alexander Vidmar <pierrialexandervidmar@gmail.com>
2022-10-26 23:00:10 -0300

    Novas alterações
    Novas Alterações

--- README.md
+++ README.md
@@ -7,5 +7,8 @@ Validador de CEP e outros a partir de arquivos Excel (em desenvolvimento)
 pip install requests <br>
 pip install selenium <br>
 pip install playwright <br>
+pip install pycep-correios <br>
+pip install numpy <br>
+pip install xlrd <br>
 
 
---
+++ versao_02/main.py
@@ -0,0 +1,64 @@
+import xlrd
+import numpy as np
+from pycep_correios import get_address_from_cep, WebService, exceptions
+
+wb = xlrd.open_workbook('CEP.xlsx')
+p = wb.sheet_by_name('Lista')
+lin = p.nrows
+array_cep = np.zeros(lin)
+
+for i in range(lin):
+    array_cep[i] = p.cell(i, 3).value
+
+#=============================================
+
+counter = 0
+erro_caracteres = 0
+ceps_inexistentes = []
+cep_qnt_invalida = []
+
+'''
+Percorre a lista com os CEPs faz a requisição e valida se existe todos os CEPs válidos.
+'''
+for cep in array_cep:
+    cep_ok = int(cep)
+    if len(str(cep_ok)) != 8:
+        erro_caracteres += 1
+        cep_qnt_invalida.append(cep_ok)
+
+    try:
+        endereco = get_address_from_cep(str(cep_ok), webservice=WebService.CORREIOS)
+        print(f"Lido: {endereco['cep']} - Rua: {endereco['logradouro']}")
+
+    except exceptions.InvalidCEP:
+        ceps_inexistentes.append(cep_ok)
+
+    except exceptions.CEPNotFound:
+        ceps_inexistentes.append(cep_ok)
+        counter += 1
+
+    except exceptions.ConnectionError as errc:
+        print(f'Erro de conexão com o servidor dos correios: {errc}')
+
+    except exceptions.Timeout as errt:
+        print(f'Erro de timeout: {errt}')
+
+    except exceptions.HTTPError as errh:
+        print(f'Erro de HTTPt: {errt}')
+
+    except exceptions.BaseException as e:
+        ceps_inexistentes.append(cep_ok)
+        counter += 1
+
+
+
+print('*' * 30)
+print(f'CEPs com caracteres inválidos: {erro_caracteres}')
+print(f'LISTA de CEPs com quantidade de caracteres incorretos: {cep_qnt_invalida}')
+print('*' * 30)
+print(f'A quantidade de CEPs não encontrados no WebService foi de: {counter}')
+print(f'LISTA de CEPs inexistentes no WebService: {ceps_inexistentes}')
+print('*' * 30)
+
+
+
